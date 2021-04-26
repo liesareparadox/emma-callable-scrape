715a741d616ea36057990f3b280846c6a4ba5505
liesareparadox <78282871+liesareparadox@users.noreply.github.com>
2021-04-25 23:05:50 -0400

    Update main.py
    added write to csv method.

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import logging
+from time import sleep
 import time
 
 from WebscraperFuntion import DataScraper
+from WebscraperFuntion import ChromeWebDriver
 
 if __name__ == '__main__':
-    state = 'AK'
-    bond_purpose = '100'
     start_date = '03/01/2021'
-    end_date = '04/01/2021'
+    end_date = '03/02/2021'
+    trade_yield_min = 0
+    trade_yield_max = 6
 
-    muni_search = DataScraper()
+    # Go to Emma site setup initial search parameters and create the state iterable list
+    muni_search = ChromeWebDriver()
 
     muni_search.go_to_emma_and_accept_disclaimer()
 
     muni_search.specify_callable_yes()
 
+    muni_search.specify_tax_exempt()
+
+    muni_search.set_trade_yields(trade_yield_min, trade_yield_max)
+
     muni_search.specify_search_date(start_date, end_date)
+
+    states = muni_search.get_state_list()
+
+    muni_search.click_on_state(states['AK'])
 
     muni_search.run_search()
 
-    muni_search.change_view_to_be_by_issuers()
+    muni_search.click_display_results_by_100()
 
-    muni_search.select_purpose_of_muni_bond(bond_purpose)
+    # setup data scraper
+    data_scraper = DataScraper(muni_search.driver)
 
-    time.sleep(0)
+    data_scraper.create_document_table()
 
-    muni_search.driver.close()
+    data_scraper.copy_data_and_iterate_through_pages()
+
+    data_scraper.write_csv_file()
+
+    data_scraper.driver.close()
